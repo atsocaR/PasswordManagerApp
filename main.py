c0018e1c8127290cd27f3d2b2fb0b462eb6eaea8
Edgar <atsoca_ragde@hotmail.com>
2021-05-31 19:54:46 -0500

    ui labels added: website, email/username, password

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ lock_image = PhotoImage(file="logo.png")
 # Canvas
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=lock_image)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
 
+# Labels
+website_label = Label(text='Website:')
+website_label.grid(column=0, row=1)
 
+email_username_label = Label(text='Email/Username:')
+email_username_label.grid(column=0, row=2)
 
-
+password_label = Label(text='Password:')
+password_label.grid(column=0, row=3)
 
 
 
